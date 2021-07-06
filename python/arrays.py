8bb928d1d42545886c35ed8d54214a51af577850
MD Parvej <55233570+Parvej240@users.noreply.github.com>
2021-07-06 14:58:40 +0600

    Add files via upload

---
+++ python/arrays.py
@@ -0,0 +1,30 @@
+cars = ["NISAN","MISUBISI","BMW","COROLA"]
+print(cars)
+
+cars = ["NISAN","MISUBISI","BMW","COROLA"]
+x = cars[0]
+print(x)
+
+cars = ["NISAN","MISUBISI","BMW","COROLA"]
+cars[0]="Raner"
+print(cars)
+
+cars = ["NISAN","MISUBISI","BMW","COROLA"]
+x=len(cars)
+print(x)
+
+Name = ["Parvej","Pavel","Buri","Tamima"]
+for x in Name:
+    print(x)
+
+Allah = ["Rahim","Karim","malik","khalik"]
+Allah.append("al_kuddus")
+print(Allah)
+
+Name = ["Parvej","Pavel","Buri","Tamima"]
+Name.pop(0)
+print(Name)
+
+Name = ["Parvej","Pavel","Buri","Tamima"]
+Name.remove("Tamima")
+print(Name)
